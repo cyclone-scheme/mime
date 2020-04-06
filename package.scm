351b6a90ea71f74ecc2cbf3ccd5be30647f58fea
Justin Ethier <justin.ethier@gmail.com>
2020-04-06 18:45:09 -0400

    Cyclone-winds package information

---
+++ package.scm
@@ -0,0 +1,15 @@
+(package
+ (name           mime)
+ (version        0.1)
+ (license        "BSD")
+ (authors        "Alex Shinn")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "A library to parse MIME headers and bodies into SXML")
+ (tags           "networking")
+ (docs           "https://github.com/cyclone-scheme/mime")
+ (test           "mime-test.scm")
+ (dependencies   (base64 quoted-printable string))
+
+ (library
+     (name (cyclone mime))
+   (description "A library to parse MIME headers and bodies into SXML")))
